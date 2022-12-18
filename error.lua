502a61c1d8a839fc480117f792af27afc20135c7
Cr0s5H34D <70684668+Cr0s5H34D@users.noreply.github.com>
2022-12-17 20:19:19 -0600

    error.lua

---
+++ error.lua
@@ -0,0 +1,23 @@
+error("HEY! LEAVE YOU UNWANTED PERSON")
+
+error("░░░█░░█░░█░░░")
+
+error("░░░█░░█░░█░░░")
+
+error("█░░▒▒▒▓▒▒▒░░█")
+
+error("▓░░░░░▓░░░░░▓")
+
+error("▒▒▒▒▓▓▓▓▓▒▒▒▒")
+
+error("░░░░░░▓░░░░░░")
+
+error("░░█▓▒▒▓▒▒▓█░░")
+
+error("░░░░░▓░▓░░░░░")
+
+error("░░░░▓░░░▓░░░░")
+
+error("░░░░░▓▓▓░░░░░")
+
+error("KEYCHAIN FOREVER")
